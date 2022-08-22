0997377176b35ae155374a54d14ee996480c3de4
Shalai Ivan <shalai.ivan30@gmail.com>
2022-08-22 09:01:16 +0300

    added Tab Bar Controller

---
+++ Training Diary/Controllers/Tab Bar/Useful/UsefulVC.swift
@@ -0,0 +1,29 @@
+//
+//  AnotherVC.swift
+//  Training Diary
+//
+//  Created by user on 19.08.2022.
+//
+
+import UIKit
+
+class UsefulVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
